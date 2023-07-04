77f2d78c90d98fd08c1ce360dd2b5dba810e79ef
Danial <124687107+selnoron@users.noreply.github.com>
2023-07-04 21:16:25 +0600

    dasf

--- apps/games/models.py
+++ apps/games/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -15,12 +17,12 @@ class Company(models.Model):
     )
 
     class Meta:
-        ordering = ('name',)
-        verbose_name = 'Компания',
-        verbose_name_plural = 'Компании'
+        ordering = ('-datetime_created',)
+        verbose_name = 'компания'
+        verbose_name_plural = 'компании'
 
     def __str__(self) -> str:
-        return self.name
+        return self.name    
 
 
 class Genre(models.Model):
@@ -34,8 +36,8 @@ class Genre(models.Model):
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'Жанр',
-        verbose_name_plural = 'Жанры'
+        verbose_name = 'жанр'
+        verbose_name_plural = 'жанры'
 
     def __str__(self) -> str:
         return self.name
@@ -70,16 +72,16 @@ class Game(models.Model):
     )
 
     class Meta:
-        ordering = ('name', )
-        verbose_name = 'Игра',
-        verbose_name_plural = 'Игры'
+        ordering = ('price', 'name')
+        verbose_name = 'игра'
+        verbose_name_plural = 'игры'
 
     def __str__(self) -> str:
         return f'{self.company} | {self.name} | {self.price}$'
 
 
-
 class Comment(models.Model):
+
     """Comment for games and comp."""
     
     user = models.ForeignKey(
@@ -97,7 +99,7 @@ class Comment(models.Model):
     )
     game = models.ForeignKey(
         verbose_name='игра',
-        related_name='games_comments',
+        related_name='game_comments',
         to=Game,
         on_delete=models.CASCADE,
         null=True,
@@ -113,10 +115,103 @@ class Comment(models.Model):
     )
 
     class Meta:
-        ordering = ('user',)
-        verbose_name = 'Комментарий',
-        verbose_name_plural = 'Коментарии'
+        ordering = ('-id',)
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
 
     def __str__(self) -> str:
-        rating = '★ ' * self.rate
-        return f'{self.user} => {rating}'
+
+        rate_str = '★' * self.rate 
+        return f'{self.user.username} оценка:   {rate_str}'
+
+    
+class Order(models.Model):
+
+    CARD='0'
+    CASH='1'
+    METHOD=(
+        (CARD,'карта'),
+        (CASH,'наличкой при доставке')
+    )
+
+    user = models.ForeignKey(
+        verbose_name='юзер',
+        to=User, 
+        on_delete=models.CASCADE,
+        related_name='order')
+    game = models.ForeignKey(
+        verbose_name='игра',
+        to=Game, 
+        on_delete=models.CASCADE,
+        related_name='order')
+    datetime_buy = models.DateTimeField(
+        verbose_name='время совершения покупки',
+        auto_now_add=True
+    )
+    method = models.CharField(
+        verbose_name='метод оплаты',
+        max_length=250,
+        choices=METHOD,
+        default=CARD
+    )
+    money = models.DecimalField(
+        verbose_name='потрачено',
+        decimal_places=2,
+        max_digits=12
+    )
+    
+    class Meta:
+        verbose_name = 'покупка'
+        verbose_name_plural = 'покупки'
+
+    def __str__(self) -> str:
+        return f'{self.user} : {self.game} : {self.method}'
+
+
+class WishList(models.Model):
+    user = models.OneToOneField(
+        verbose_name='властелин',
+        to=User, 
+        on_delete=models.CASCADE,
+        related_name='wishlist')
+    games = models.ManyToManyField(
+        verbose_name='игра',
+        to=Game,
+        related_name='wishlists')
+
+    class Meta:
+        verbose_name = 'избранный'
+        verbose_name_plural = 'избранные'
+
+    def __str__(self) -> str:
+        return f'{self.user} ||| {self.games.count()} games'
+
+
+class InviteCard(models.Model):
+
+    code = models.CharField(
+        verbose_name='код', 
+        max_length=50, 
+        unique=True
+    )
+    owner = models.ForeignKey(
+        to=User, 
+        verbose_name='владелец', 
+        on_delete=models.CASCADE
+    )
+    expiration_date = models.DateField(
+        verbose_name='дата истечения',
+        default=datetime.datetime.today() + datetime.timedelta(days=30)
+    )
+    counter = models.PositiveIntegerField(
+        verbose_name='счётчик', 
+        default=0
+    )
+
+    class Meta:
+        ordering = ('-counter',)
+        verbose_name = 'пригласительная карта'
+        verbose_name_plural = 'пригласительные карты'
+
+    def str(self) -> str:
+        return f"Код: {self.code}| Использовано: {self.is_used}"
--- apps/games/views.py
+++ apps/games/views.py
@@ -1,10 +1,12 @@
+# Django
 from django.shortcuts import render
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.db.models.query import QuerySet
 
-#Local
+# Local
 from .models import Game
+
 
 def index(request: HttpRequest) -> HttpResponse:
     template_name: str = 'games/index.html'
@@ -28,16 +30,10 @@ def about(request: HttpRequest) -> HttpResponse:
 def get_game(request: HttpRequest, game_id: int) -> HttpResponse:
     try:
         game: Game = Game.objects.get(id=game_id)
-        template_name: str = 'games/com.html'
-        qs: QuerySet[Game] = Game.objects.all()
     except Game.DoesNotExist as e:
         return HttpResponse(
-            f'<h1> The game with id as {game_id} does`t exist'
-            )
-    return render(
-        request=request,
-        template_name=template_name,
-        context={
-            'games': qs[game_id -1]
-        }
+            f'<h1>Игры с id {game_id} не существует!</h1>'
+        )
+    return HttpResponse(
+        f'<h1>ID: {game.name}</h1>'
     )
